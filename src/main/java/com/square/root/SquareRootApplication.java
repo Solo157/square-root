ab3a439015e54790d1f2fdbcb942280c20a50f04
kirill <kirill.vasyukov@eltex.loc>
2025-09-29 19:53:48 +0700

    init commit 2

---
+++ src/main/java/com/square/root/SquareRootApplication.java
@@ -0,0 +1,12 @@
+package com.square.root;
+
+//@SpringBootApplication
+public class SquareRootApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(SquareRootApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/square/root/SquareRootApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.square.root;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SquareRootApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
